aff75a0f409aba15f6b91070f30aaf49e7696b49
John Walters <johnwalters@Johns-Mac-mini.local>
2018-01-21 09:04:00 -0800

    editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -12,7 +12,8 @@ import { ProductComponent } from './product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,15 +7,26 @@ import { Product } from '../product';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  MODE_EDIT:string = "edit";
+  MODE_VIEW:string = "view";
+
   product: Product = {
     asin: "B01887A4E4",
     keywordListText: "serving tray\nserving trays\ndecorative serving trays\nclear tray"
   };
+  editMode:string = this.MODE_EDIT;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSave():void{
+    this.editMode = this.MODE_VIEW;
+  }
 
+  onEnterEditMode():void{
+    this.editMode = this.MODE_EDIT;
+  }
 
 }
